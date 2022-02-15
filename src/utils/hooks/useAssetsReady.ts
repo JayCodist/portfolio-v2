dc6393f8297f1056bd1033b499b6d17b8c074cc2
Johnson Ubaezuonu <jaycodist@gmail.com>
2022-02-15 13:38:31 +0100

    added custom hook for prefetching images

---
+++ src/utils/hooks/useAssetsReady.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const preloadImage: (imageUrl: string) => Promise<void> = imageUrl =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => resolve();
+    img.onerror = () => reject();
+  });
+
+/**
+ * Custom hook for prefetching assets on a page
+ * @param imageUrls Array of image urls
+ * @returns boolean indicating if all images are loaded
+ */
+const useAssetsReady: (imageUrls: string[]) => boolean = imageUrls => {
+  const [allLoaded, setAllLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleAssetsPreload = async () => {
+      try {
+        const responses = imageUrls.map(asset => preloadImage(asset));
+        await Promise.all(responses);
+        setAllLoaded(true);
+      } catch (error) {
+        console.error("Unable to load assets: ", error);
+      }
+    };
+
+    handleAssetsPreload();
+  }, [imageUrls]);
+
+  return allLoaded;
+};
+
+export default useAssetsReady;
